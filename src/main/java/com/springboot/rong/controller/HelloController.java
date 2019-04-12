a57f34284cde8e11b53d0e2b8923188e2fcb0b4e
810209659@qq.com <810209659@qq.com>
2019-04-12 11:10:12 +0800

    Restful

--- src/main/java/com/springboot/rong/controller/HelloController.java
+++ src/main/java/com/springboot/rong/controller/HelloController.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,4 +31,20 @@ public class HelloController {
         UserDTO user = this.userService.getUserById(userId);
         return user;
     }
+    
+	/*Restful就是一个资源定位及资源操作的风格，不是标准也不是协议，只是一种风格，是对http协议的诠释。
+	资源定位：互联网所有的事物都是资源，要求url中没有动词，只有名词，没有参数。url请求的风格就像这样：
+	http://blog.csdn.net/eson_15/article/details/51743514
+	@RequestMapping(value="/itemEdit/{id}")：{×××}表示占位符，请求的URL可以是“/itemEdit/1”或“/itemEdit/2”，
+	通过在方法中使用@PathVariable获取{×××}中的×××变量。@PathVariable用于将请求URL中的模板变量映射到功能处理方法的参数上。
+	如果@RequestMapping中表示为”/viewItems/{id}”，id和形参名称一致，那么@PathVariable就不用指定名称。 
+	*/   
+    
+    @RequestMapping("/Restful/{id}")
+    public UserDTO Restful(@PathVariable("id") Integer iid, Model model){
+        UserDTO user = this.userService.getUserById(iid);
+        return user;
+    }
+  
+ 
 }
