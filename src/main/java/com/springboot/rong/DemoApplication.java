227c29f0dc5bb62039eb0c364e98e4f2e553f157
810209659@qq.com <810209659@qq.com>
2019-04-23 14:25:19 +0800

    忽略提交

--- src/main/java/com/springboot/rong/DemoApplication.java
+++ src/main/java/com/springboot/rong/DemoApplication.java
@@ -1,11 +1,10 @@
 package com.springboot.rong;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@MapperScan(value = {"com.srpingboot.rong"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
